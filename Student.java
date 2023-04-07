8c3a94f99d0c1631059709f9a5cba39e6b924bd3
LEE HUA XUEN <TP063031@mail.apu.edu.my>
2023-04-07 16:02:56 +0800

    1st pull

---
+++ Student.java
@@ -0,0 +1,24 @@
+public class Student extends User{
+    private int studentID;
+    private int level;
+
+    private void login(){
+    
+        String str = "test1|test2";
+        String[] splitstr = str.split("|");
+        if (splitstr[0] == "test1"){
+            S
+        }
+
+
+    }
+
+
+
+    // Hostel hosteldata;
+    // FileHandling filehandler;
+    // hosteldata.records = filehandler.load("records");
+
+    // Student student;
+    // student.data = filehandler.load("studentrecords");
+}
